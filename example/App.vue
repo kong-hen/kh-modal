e3624b0a7ed0de80d61d97b9da410a61258f0b83
kong-hen <100201531+kong-hen@users.noreply.github.com>
2023-06-18 17:53:37 +0800

    uniapp项目文件
    uniapp项目文件

---
+++ example/App.vue
@@ -0,0 +1,19 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/* 导入弹窗组件css */
+	@import url('components/kh-modal/modal.css');
+	/*每个页面公共css */
+</style>
